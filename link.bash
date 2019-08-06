100591f3b6c620c2c2a64430df13cf229200547c
vincent delmas <delmasv@vincents-iMac.local>
2019-08-05 19:54:55 -0700

    linking script

---
+++ link.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DOT_PATH=`pwd`
+ln -sf $DOT_PATH/.bashrc ~/.bashrc
+ln -sf $DOT_PATH/.bash_profile ~/.bash_profile
+ln -sf $DOT_PATH/.bash_aliases ~/.bash_aliases
+ln -sf $DOT_PATH/.vimrc ~/.vimrc
+ln -sf $DOT_PATH/.tmux.conf ~/.tmux.conf
