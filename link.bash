edc95ec6d14b5e1018ea44262c6797f99f766a56
Vincent Delmas <vincent.delmas@hotmail.fr>
2019-12-12 02:24:48 -0800

    updated link

--- link.bash
+++ link.bash
@@ -6,3 +6,4 @@ ln -sf $DOT_PATH/.bash_profile ~/.bash_profile
 ln -sf $DOT_PATH/.bash_aliases ~/.bash_aliases
 ln -sf $DOT_PATH/.vimrc ~/.vimrc
 ln -sf $DOT_PATH/.tmux.conf ~/.tmux.conf
+ln -sf $DOT_PATH/.vim ~/.vim
